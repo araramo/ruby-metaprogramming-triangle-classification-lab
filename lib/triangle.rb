d6d76de1cba87ba94cfaa748e4fa6eee04a8e956
Araceli Ramos <celi2455@gmail.com>
2022-05-25 17:31:12 -0500

    Completed assignment

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,31 @@
 class Triangle
-  # write code here
+  attr_accessor :a, :b, :c
+
+  def initialize(a, b, c)
+    @a = a
+    @b = b
+    @c = c
+  end
+
+  def kind
+    valid_triangles
+    if a == b && b == c
+      :equilateral
+    elsif a == b || a == c || b == c
+      :isosceles
+    else
+      :scalene
+    end
+  end
+
+  def valid_triangles
+    true_triangle = [(a + b > c), (a + c > b), (b + c > a)]
+    [a, b, c].each do |side|
+      true_triangle << false if side <= 0 
+      raise TriangleError if true_triangle.include?(false)
+    end
+  end
+
+  class TriangleError < StandardError
+  end
 end
